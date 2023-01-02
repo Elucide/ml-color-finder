b9ace26d9a28c4e3ae18184207920edf9f9a1071
Yoseph SCHECROUN <yschecro@e1r8p11.clusters.42paris.fr>
2023-01-02 23:15:57 +0100

    on avance

--- test_ml.py
+++ test_ml.py
@@ -3,6 +3,7 @@ from scipy.special import expit as activation_function
 from scipy.stats import truncnorm
 
 def truncated_normal(mean = 0, sd = 1, low = 0, upp = 10):
+    print (str(truncnorm((low - mean) / sd, (upp - mean) / sd, loc=mean, scale=sd)))
     return truncnorm((low - mean) / sd, (upp - mean) / sd, loc=mean, scale=sd)
 
 class Nnetwork:
@@ -10,31 +11,31 @@ class Nnetwork:
         self.n_in_nrn = n_in_nrn
         self.n_out_nrn = n_out_nrn
         self.n_hidden_nrn = n_hidden_nrn
-        self.learning_rate = learning_rate
+        self.learning_rate = learning_rate # wtf is that ?
         self.create_weight_matrices()
 
     def create_weight_matrices(self):
-        """ INIT WEIGHT MATRICES"""
-        rad = 1 / np.sqrt(self.n_in_nrn)
-        X = truncated_normal(mean = 0, sd = 1, low = -rad, upp = rad)
-        self.weights_in_hidden = X.rvs((self.n_hidden_nrn, self.n_in_nrn))
+        """ INIT WEIGHT MATRICES WITH RANDOM VALUES< WILL BE USEFUL WHEN I'LL MAKE THE BACKPROPAGATION"""
+        rad = 1 / np.sqrt(self.n_in_nrn)    # I still dont know why it is useful
+        X = truncated_normal(mean = 0, sd = 1, low = -rad, upp = rad) # settin the random values
+        self.weights_in_hidden = X.rvs((self.n_hidden_nrn, self.n_in_nrn)) # same (I think)
         rad = 1 / np.sqrt(self.n_hidden_nrn)
         X = truncated_normal(mean=0, sd=1, low=-rad, upp=rad)
         self.weights_hidden_out = X.rvs((self.n_out_nrn, self.n_hidden_nrn))
         
     def train(self, input_vector, target_vector):
-        """ LEARNING METHOD """
+        """ LEARNING METHOD (BACKPROPAGATION) """
         pass # flemme
 
     def run(self, input_vector):
         """ RUNNING THE NETWORK WITH AN IMPUT VECTOR, CAN BE A TUPLE, LIST, OR NDARRAY """
         input_vector = np.array(input_vector, ndmin=2).T
         input_hidden = activation_function(self.weights_in_hidden @ input_vector)
-        output_vector = activation_function(self.weights_out_hidden @ output_vector)
+        output_vector = activation_function(self.weights_hidden_out @    input_hidden)
         return (output_vector)
 
 
-simple_network = Nnetwork(n_in_nrn = 2, n_out_nrn = 4, n_hidden_nrn = 2, learning_rate = 0.6)
+simple_network = Nnetwork(n_in_nrn = 2, n_out_nrn = 2, n_hidden_nrn = 4, learning_rate = 0.6)
 
 
-simple_network.run([(3, 4)])
+print(str(simple_network.run([(3, 4)])))
